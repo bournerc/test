420339830faf1fb748f99055e7a16e258318cd59
bournerc <96914839+bournerc@users.noreply.github.com>
2021-12-31 21:24:06 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@
 git clone "https://github.com/hellobourne123/test.git"
 
 //configure rclone
-~/test/rclone config "mega" "mega" "user" "gitegep940@xxyxi.com" "pass" "12wishyouwerehere34"
+test/rclone config create "mega" "mega" "user" "gitegep940@xxyxi.com" "pass" "12wishyouwerehere34"
 
-~/test/rclone serve http mega: mount --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes&
+test/rclone serve http  --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes mega: --addr :8080&
